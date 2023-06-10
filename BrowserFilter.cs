a6018d3d8c49c69c9e82d4054f150498d88a0542
YorVeX <528974+YorVeX@users.noreply.github.com>
2023-06-10 04:31:11 +0200

    Fixed #2 (refreshing not starting directly after filter add)

--- BrowserFilter.cs
+++ BrowserFilter.cs
@@ -149,6 +149,9 @@ public class BrowserFilter
     fixed (byte* intervalId = "interval"u8)
       context->RefreshIntervalSeconds = (int)ObsData.obs_data_get_int(settings, (sbyte*)intervalId);
     Module.Log("Browser auto refresh interval was set to " + context->RefreshIntervalSeconds + " second(s)", ObsLogLevel.Debug);
+    
+    var browserSource = Obs.obs_filter_get_parent(context->Source);
+    context->Active = (browserSource != null) && Convert.ToBoolean(Obs.obs_source_active(browserSource)); // this ensures any newly added filter to immediately start refreshing if the browser source is visible
   }
 
   [UnmanagedCallersOnly(CallConvs = new[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
